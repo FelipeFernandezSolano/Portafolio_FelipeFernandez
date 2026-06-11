9d71947e483c312943775eb1ed4f8aba1e72f498
FelipeFernandezSolano <lfernandez40990@ufide.ac.cr>
2026-06-10 20:00:47 -0600

    Semana 4
    Firebase y todo funcionanando

---
+++ TechShop_Felipe/src/main/java/TechShop/Felipe/repository/ProductoRepository.java
@@ -0,0 +1,13 @@
+package TechShop.Felipe.repository;
+
+import TechShop.Felipe.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+    public List<Producto> findByActivoTrue();
+
+}
