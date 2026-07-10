565e4bd7a01b25e416e090313a21b0bcccf44015
FelipeFernandezSolano <lfernandez40990@ufide.ac.cr>
2026-07-09 18:31:52 -0600

    Semana 9

--- TechShop_Felipe/src/main/java/TechShop/Felipe/ProjectConfig.java
+++ TechShop_Felipe/src/main/java/TechShop/Felipe/ProjectConfig.java
@@ -1,10 +1,19 @@
 
 package TechShop.Felipe;
+
 import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -17,6 +26,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
 
+    /* Los siguiente metodos son para implementar el tema de seguridad dentro del proyecto */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
@@ -24,6 +34,7 @@ public class ProjectConfig implements WebMvcConfigurer {
         registry.addViewController("/multimedia").setViewName("multimedia");
         registry.addViewController("/iframes").setViewName("iframes");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/acceso_denegado").setViewName("acceso_denegado");
         registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
     }
 
@@ -65,5 +76,82 @@ public class ProjectConfig implements WebMvcConfigurer {
         messageSource.setBasename("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    public static final String[] PUBLIC_URLS = {
+        "/", "/index", "/fav/**", "/carrito/**", "/consultas/**", "/registro/**",
+        "/js/**", "/css/**", "/webjars/**", "/login", "/acceso_denegado"
+    };
+
+    public static final String[] ADMIN_URLS = {
+        "/producto/nuevo", "/producto/guardar", "/producto/modificar/**", "/producto/eliminar/**",
+        "/categoria/nuevo", "/categoria/guardar", "/categoria/modificar/**", "/categoria/eliminar/**",
+        "/usuario/nuevo", "/usuario/guardar", "/usuario/modificar/**", "/usuario/eliminar/**"
+    };
+
+    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
+        "/producto/listado", "/categoria/listado", "/usuario/listado"
+    };
+
+    public static final String[] USUARIO_URLS = {
+        "/facturar/carrito"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers(PUBLIC_URLS).permitAll()
+                .requestMatchers(ADMIN_URLS).hasRole("ADMIN")
+                .requestMatchers(ADMIN_OR_VENDEDOR_URLS).hasAnyRole("ADMIN", "VENDEDOR")
+                .requestMatchers(USUARIO_URLS).hasRole("USUARIO")
+                .anyRequest().authenticated()
+        ).formLogin(form -> form
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+        ).logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+        ).exceptionHandling(exceptions -> exceptions
+                .accessDeniedPage("/acceso_denegado")
+        ).sessionManagement(session -> session
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+        );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Este metodo sera reemplazado la siguiente semana
+    @Bean
+    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username("juan")
+                .password(passwordEncoder.encode("123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails sales = User.builder()
+                .username("rebeca")
+                .password(passwordEncoder.encode("456"))
+                .roles("VENDEDOR")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("pedro")
+                .password(passwordEncoder.encode("789"))
+                .roles("USUARIO")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, sales, user);
     }
 }
